ea48ebef4b94c5e847ec32928a5540d40be01ac2
ochirpurev20 <oscaravna2020@gmail.com>
2020-05-27 16:11:27 +0800

    state bolon search object uusgelee

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,30 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
+/* 
+web app tuluv
+hailtiin query, ur dun
+tuhain uzuulj baigaa jor
+like darsan joruud
+zahialj baigaa joriin orts
+*/
+const state = {};
+const controlSearch = async () => {
+  //console.log("daragdlaa ");
+  //web s hailtiin tulhuur ugiig gargaj avna
+  const query = "pizza";
 
-let search = new Search("pizza");
-
-search.doSearch().then((r) => console.log(r));
+  if (query) {
+    //tulhuur ugeer haisan object iig shineer uusgene
+    state.search = new Search(query);
+    //hailt hiihed zoriulj ui beltgene
+    //hailtiig hiine
+    await state.search.doSearch();
+    //hailtiin ur dung delgetsend gargana
+    console.log(state.search.result);
+  } else {
+  }
+};
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
